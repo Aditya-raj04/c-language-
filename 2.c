f71ae452d4c5837cb20d2090f1bf5d6d9d5e8574
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2025-12-06 12:42:30 +0000

    c lang practice 2

---
+++ 2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    // short int a = -3;
+    // signed short int b = 15000;
+
+    // printf("a = %u\n", a);
+    // printf("b = %d\n", b);
+
+    // int i = 0;
+    // do{
+    //     printf("%d\n",i);
+    //     i++;
+    // }while (i<10);
+
+
+    // int i = 1;
+    // int sum = 0;
+    // while (i<=10)
+    // {
+    //     sum= sum+ i;
+    //     i++;
+    // }
+    // printf("sum=%d\n",sum);
+    
+//     int sum = 0;
+// for (int i = 0; i <= 10; i++)
+// {
+//     sum = sum + i;
+// }
+// printf("%d\n", sum);
+
+// int i = 1;
+// int sum = 0;
+// do
+// {
+//     sum = sum + i;
+//     i++;
+// } while (i<=11);
+// printf("sum=%d\n",sum);
+
+// 8 ke table ka sum 
+// int num = 8;
+// int sum = 0;
+// for (int i = 1; i <= 10; i++)
+// {
+//      sum += (num * i);
+// }
+// printf("Sum of 8 times table is: %d\n", sum);
+
+// factorial of a number
+int product = 1;
+for (int i = 1; i <= 0; i++)
+{
+    product *= i;
+
+        // printf("Intermediate product after multipl/ying %d is: %d\n", i, product);
+}
+printf("Sum is: %d\n", product);
+
+
+    return 0;
+}
